609e90cd32ec0278f00c5ad6d01ce9744db9b0c8
Darius Cepulis <dcepulis@microsensorlabs.com>
2021-03-22 14:02:33 -0500

    Favicon

--- src/App.svelte
+++ src/App.svelte
@@ -3,15 +3,6 @@
   import PlaysTable from "./PlaysTable.svelte";
 </script>
 
-<main>
-  <h1>Formula Fun</h1>
-  <OddsTable />
-  <PlaysTable />
-</main>
-
-<style>
-  main {
-    padding-bottom: 1rem;
-    padding-bottom: max(env(safe-area-inset-bottom), 1rem);
-  }
-</style>
+<h1>Formula Fun</h1>
+<OddsTable />
+<PlaysTable />
