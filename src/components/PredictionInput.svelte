ba28f628bb2e28c9f24fe9c5445e427d8e6ed74d
Darius Cepulis <dcepulis@microsensorlabs.com>
2021-06-26 07:14:56 -0500

    Fix Prediction Table

--- src/components/PredictionInput.svelte
+++ src/components/PredictionInput.svelte
@@ -11,7 +11,7 @@
         ([, { cost: costA }], [, { cost: costB }]) => costB - costA
       );
 
-      const scores = [20, 18, 16, 14, 12, 10, 8, 6, 3, 2, 1];
+      const scores = [20, 18, 16, 14, 12, 10, 8, 6, 4, 3, 2, 1];
       const initialPredictionEntries = costTableEntries.map(
         ([driver], index) => [driver, scores[index] ?? 0]
       );
